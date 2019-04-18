bb6931c328c3611d6be4d1e6ea7034118a555818
taras kolomoets <zulteg@gmail.com>
2019-04-18 10:54:28 +0300

    reset HW2

--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -1,18 +1,9 @@
 from typing import List, Dict, Union, Generator
-import sys
-import string
-import random
 
 # We will work with such dicts
 ST = Dict[str, Union[str, int]]
 # And we will put this dicts in list
 DT = List[ST]
-
-
-def format_item(item):
-    if 'name' in item:
-        item['name'] = item.get('name').capitalize()
-    return item
 
 
 def task_1_fix_names_start_letter(data: DT) -> DT:
@@ -23,13 +14,7 @@ def task_1_fix_names_start_letter(data: DT) -> DT:
         fix_names_start_letters([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}])
         >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
     """
-    return list(map(format_item, data))
-
-
-def rm_item_keys(item, keys):
-    for key in keys:
-        item.pop(key, None)
-    return item
+    pass
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
@@ -40,7 +25,7 @@ def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
        remove_dict_field([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
         >>> [{'name': 'Alex'}, {'name': 'denys'}]
     """
-    return [rm_item_keys(item, redundant_keys) for item in data]
+    pass
 
 
 def task_3_find_item_via_value(data: DT, value) -> DT:
@@ -50,21 +35,21 @@ def task_3_find_item_via_value(data: DT, value) -> DT:
         find_item_via_value([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
         >>> [{'name': 'Alex', 'age': 26}]
     """
-    return list(filter(lambda x: value in x.values(), data))
+    pass
 
 
 def task_4_min_value_integers(data: List[int]) -> int:
     """
     Find and return minimum value from list
     """
-    return min(data, default=None)
+    pass
 
 
 def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     """
     Find the longest string
     """
-    return str(min(data, key=lambda x: len(str(x)))) if len(data) > 0 else None
+    pass
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
@@ -73,22 +58,21 @@ def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
     Returns:
 
     """
-    return min(data, key=lambda x: x.get(key, sys.maxsize))
+    pass
 
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     """
     Find max value from list of lists
     """
-    new_data = list(filter(lambda x: x, data))
-    return max(max(new_data, key=lambda x: min(x)))
+    pass
 
 
 def task_8_sum_of_ints(data: List[int]) -> int:
     """
     Find sum of all items in given list
     """
-    return sum(data)
+    pass
 
 
 def task_9_sum_characters_positions(text: str) -> int:
@@ -104,22 +88,7 @@ def task_9_sum_characters_positions(text: str) -> int:
         >>> 532
 
     """
-    return sum(map(lambda x: ord(x), text))
-
-
-def is_prime(number):
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-    return True
-
-
-def generator_of_prime_numbers():
-    number = 2
-    while number <= 200:
-        if is_prime(number):
-            yield number
-        number += 1
+    pass
 
 
 def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
@@ -133,7 +102,7 @@ def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
         next(a)
         >>> 3
     """
-    return generator_of_prime_numbers()
+    pass
 
 
 def task_11_create_list_of_random_characters() -> List[str]:
@@ -141,4 +110,4 @@ def task_11_create_list_of_random_characters() -> List[str]:
     Create list of 20 elements where each element is random letter from latin alphabet
 
     """
-    return [string.ascii_lowercase[random.randint(0, len(string.ascii_lowercase) - 1)] for _ in range(0, 20)]
+    pass
