147f86a43879989cea04e70802be099943c6140d
Taras Kolomoiets <zulteg@gmail.com>
2019-05-10 16:09:34 +0300

    homework 5

---
+++ serialization/constants.py
@@ -0,0 +1,4 @@
+CARS_TYPES = ["SUV", "Truck", "Sedan", "Van", "Coupe", "Wagon", "Sports Car", "Diesel", "Crossover", "Luxury Car"]
+CARS_PRODUCER = ["BENTLEY", "BMW", "Bugatti", "Buick", "Chery", "Chevrolet", "Dodge", "Ford", "Lamborghini"]
+
+TOWNS = ["Amsterdam", "Kiev", "Prague", "Rome", "Paris", "London", "Berlin"]
