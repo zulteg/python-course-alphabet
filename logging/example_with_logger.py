87b8d4b9ef07a9dd19fce9809689a9e57a8d5813
dvasilov <dvasilov@echoua.com>
2019-05-24 21:03:23 +0300

    Update logging code

--- logging/example_with_logger.py
+++ logging/example_with_logger.py
@@ -5,6 +5,7 @@ def setup_logger():
     cursor_logger = logging.getLogger("cursor")
     cursor_logger.setLevel("INFO")
     c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler()
     c_format = logging.Formatter('%(levelname) -10s;message time: %(asctime)s; Module where message was raised: %(module)s: Number of line where message was raised: %(lineno)s Name of func where m was raised: %(funcName)s %(message)s')
     c_handler.setFormatter(c_format)
     cursor_logger.addHandler(c_handler)
